76d3d9ab22e62bad887999d5a903de8d22ebae53
NextGenerationcloud <inkognito2004@gmx.at>
2017-07-22 08:59:46 +0200

    test
    Versuch

---
+++ templates/settings/index.php
@@ -0,0 +1,12 @@
+<div id="app-settings">
+	<div id="app-settings-header">
+		<button class="settings-button"
+				data-apps-slide-toggle="#app-settings-content"
+		></button>
+	</div>
+	<div id="app-settings-content">
+		<!-- Your settings in here -->
+      <h1>Hallo<h1>
+
+	</div>
+</div>
